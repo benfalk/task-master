56081a162e7c914ad99af0a8075f25aafd7725f6
Benjamin Falk <benjamin.falk@yahoo.com>
2016-06-19 19:00:30 -0500

    empty project list, needs work

--- web/elm/App.elm
+++ web/elm/App.elm
@@ -1,6 +1,63 @@
 module App exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.App as Html
+import Html.Events exposing (..)
+import Random
 
 main =
-  text "Testing it!"
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+-- MODEL
+type alias Project =
+  { name : String
+  , id : Int
+  }
+
+type alias Model =
+  { projects : List Project
+  }
+
+init : (Model, Cmd Msg)
+init =
+  (Model [], Cmd.none)
+
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+-- UPDATE
+type Msg
+  = Add Project
+  | Remove Project
+  | Update Project
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Add project ->
+      ({ model | projects = project :: model.projects }, Cmd.none)
+
+    Remove project ->
+      ({ model | projects = List.filter (\p -> p.id /= project.id) model.projects}, Cmd.none)
+
+    Update project ->
+      ({ model | projects = List.map (\p -> if p.id == project.id then project else p) model.projects}, Cmd.none)
+
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+  div []
+    [ h1 [] [ text "Projects..." ]
+    ]
